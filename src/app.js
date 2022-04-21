468ba01d6a07325cbdf8380200b44b9a7e575334
MarijaTRastija <101635462+MarijaTRastija@users.noreply.github.com>
2022-04-21 18:45:04 +0200

    Remove console.log, added city in the link

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
@@ -38,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = `919fb7ba7dc6a5f22ab47490fe852f46`;
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Ney York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${City}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
